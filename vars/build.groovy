f6eff912b306640c5aec3f445a1918052627093e
Harshil bhardwaj <57087065+Harshilbhardwaj47@users.noreply.github.com>
2021-04-06 17:46:20 -0700

    Update build.grovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,1 +1,12 @@
 
+def check(Map config=[:], Closure body={}){
+
+check([$class: 'GitSCM',
+
+          branches: [[name: config.branch]],
+
+          extensions: [],
+
+          userRemoteConfigs: [[url: config.repo ]]])
+
+}
