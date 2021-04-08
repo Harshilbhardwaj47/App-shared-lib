34d4ef7bfb7df0f74f19b84f301c6f4e7a4bf7f4
Harshil bhardwaj <57087065+Harshilbhardwaj47@users.noreply.github.com>
2021-04-08 03:44:00 -0700

    Update status.groovy

--- vars/status.groovy
+++ vars/status.groovy
@@ -9,3 +9,15 @@ checkout([$class: 'GitSCM',
           userRemoteConfigs: [[url: config.repo ]]])
 
 }
+
+def build(Map config=[:], Closure body={}){
+
+checkout([$class: 'GitSCM',
+
+          branches: [[name: config.branch]],
+
+          extensions: [],
+
+          userRemoteConfigs: [[url: config.repo ]]])
+
+}
