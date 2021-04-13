f074e3b3618625e99e5eb98e1fde766436905672
Harshil bhardwaj <57087065+Harshilbhardwaj47@users.noreply.github.com>
2021-04-13 03:00:12 -0700

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,7 @@
 
 def check(Map config=[:], Closure body={}){
- bat 'mvn clean install pom.xml'
+ bat 'mvn %config.command1%  pom.xml'
+ bat 'mvn %config.command2%  pom.xml'
+ bat 'mvn %config.command3%  pom.xml'
 
 }
